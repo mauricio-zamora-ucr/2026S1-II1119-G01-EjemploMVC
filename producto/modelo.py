e1aeccc09e5c05c4b4dffdea75a6d875a8da8b67
Mauricio Andrés Zamora Hernández <64853106+mauricio-zamora-ucr@users.noreply.github.com>
2026-06-02 21:50:32 +0000

    x

--- producto/modelo.py
+++ producto/modelo.py
@@ -6,3 +6,17 @@ class Producto(TypedDict):
     precio: float
     cantidad: int
     activo: bool
+
+_productos:dict[str, Producto] = {}
+
+def verificar_codigo_producto(codigo:str) -> bool:
+    return codigo in _productos
+
+def agregar_producto(codigo:str, nombre:str, precio:float, cantidad:int, activo:bool) -> bool, str:
+    codigo = codigo.strip().upper()
+    if verificar_codigo_producto(codigo):
+        return False, 'El codigo ya existe'
+    else:
+        _productos[producto['codigo']] = producto
+        return True, 'Producto agregado exitosamente'
+
