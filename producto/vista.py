279d14c91530b21c51a0db9774b42f46d2aa23be
Mauricio Andrés Zamora Hernández <64853106+mauricio-zamora-ucr@users.noreply.github.com>
2026-06-02 21:53:35 +0000

    x

--- producto/vista.py
+++ producto/vista.py
@@ -1,7 +1,14 @@
-from eii_utils import mostrar_menu, pausar
+from eii_utils import mostrar_menu, pausar, leer_entero, leer_flotante, leer_booleano, leer_texto
 
 def crear_vista_menu_productos() -> int:
     return mostrar_menu('Menu Productos', 
     ['Agregar producto', 'Modificar producto', 'Eliminar producto', 'Listar productos','Reporte productos'],
     'Volver al menu principal')
 
+def crear_vista_agregar_producto() -> tuple[str, str, float, int, bool]:
+    codigo = leer_texto('Ingrese el codigo del producto: ')
+    nombre = leer_texto('Ingrese el nombre del producto: ')
+    precio = leer_flotanteinput('Ingrese el precio del producto: ')
+    cantidad = leer_entero('Ingrese la cantidad del producto: ')
+    activo = leer_booleano('¿El producto esta activo?')
+    return codigo, nombre, precio, cantidad, activo
