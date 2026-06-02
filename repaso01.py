1bb8b32d614dda4e5e047b68aa1f1935252e2911
Mauricio Andrés Zamora Hernández <64853106+mauricio-zamora-ucr@users.noreply.github.com>
2026-06-02 19:59:43 +0000

    x

--- repaso01.py
+++ repaso01.py
@@ -1,3 +1,9 @@
+def segundo_elemento(x):
+    return x[1]
+
+def filtro(x):
+    return x[1] == 8
+
 def practicar():
     lista_nombres:list[str] = ['VerOnica','roberto','JOSE','   Kimberly','Mauricio Andres   ','Cris']
     lista_nombres_limpios:list[str] = []
@@ -11,7 +17,11 @@ def practicar():
     lista_largos:list[int] = [ len(x) for x in lista_nueva ]
 
     lista_nombre_largos = list(zip(lista_nueva,lista_largos))
+    lista_nombre_largos.sort(key=segundo_elemento)
     print(lista_nombre_largos)
+
+    lista_filtrada = list(filter(filtro, lista_nombre_largos))
+    print(lista_filtrada)
 
 if __name__ == "__main__":
     practicar()
